b062222614e135e9234eaaf28ebcabf0116f8f80
User <User@FDC7M82>
2016-11-01 16:50:10 +0100

    changes nov 1

--- Udacity/src/lesson2ControlFlowAndConditionals/IfStatements.java
+++ Udacity/src/lesson2ControlFlowAndConditionals/IfStatements.java
@@ -1,4 +1,4 @@
-package lesson2;
+package lesson2ControlFlowAndConditionals;
 
 public class IfStatements {
 
---
+++ Udacity/src/lesson3Functions/Main.java
@@ -0,0 +1,12 @@
+package lesson3Functions;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		RollDice.createRandomNumber();
+		
+
+	}
+
+}
